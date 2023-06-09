d0a6bfe46a05e065c5a71555ef668f853bf6207f
KobayashiYusuke-s1300240 <136042171+KobayashiYusuke-s1300240@users.noreply.github.com>
2023-06-09 12:44:51 +0900

    Dice game: initial revision

---
+++ Daicegame.java
@@ -0,0 +1,13 @@
+  Inport java.util.Random;
+    class Daicegame{
+        public static void = new Random();
+           int num1 = rand.nextInt(6)+1;
+           int num2 = rand.nextInt(6)+1;
+
+           System.out.println("Rolling dice...");
+           System.out.println("Die 1: "+num1);
+           System.out.println("Die 2: "+num2);
+           int num = num1+num2;
+           System.out.println("Total value: "+num);
+      }
+  }
